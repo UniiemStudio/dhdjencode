63a9250c9d217e91adab4ff512bd796e6347cbbe
lixworth <lixworth@gmail.com>
2019-03-31 14:24:55 +0800

    Update dhdjencode for php

---
+++ php/README.md
@@ -0,0 +1,22 @@
+# dhdjencode For PHP
+
+dhdjencode是一个高性能的高安全性加密算法（由著名架构师、算法师dhdj开发）
+
+这个慈善项目(大喊大叫基金会)将会把dhdjencode使用各种语言实现。这必将是算法界的改革。
+
+## How to use
+
+```
+<?php
+
+include "dhdjencode.php";
+
+echo dhdjencode("Hacking to the Gate.");
+
+?>
+
+```
+
+## License
+
+Dhdjencode is open-source software licensed under the [MIT license](https://opensource.org/licenses/MIT).
---
+++ php/dhdjencode.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Dhdjencode
+ * @author dhdj
+ * @copyright @dhdjfoundation
+ * @param $str
+ * @return string
+ */
+function dhdjencode($str){
+    $content = md5(sha1(base64_encode(md5(urlencode(md5(sha1(sha1(md5(md5(urlencode(md5(strlen($str)).md5($str).md5(substr($str,0,1).md5(md5($str)).urlencode($str)))))))).sha1(sha1($str)))))).sha1(md5(urlencode(base64_encode(md5("key"))))));
+    return $content;
+}
+
+//echo dhdjencode("Hacking to the Gate.");
